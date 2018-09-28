e22f585d98fbfc52ecb34721b979c593d9a974d9
fengyuemin <fengyuemin@czxy.com>
2018-09-28 15:50:18 +0800

    '无限极分类'

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -8,7 +8,13 @@ class CategoryController{
     public function index()
     {
         $model = new Category;
-        $data = $model->findAll();
+        $data = $model->findAll([
+
+            'order_by' => 'concat(path,id,"-")',
+            'order_way' => 'asc',
+            'per_page' => 99999999,     // 不翻页
+        ]);
+        
         view('category/index', $data);
     }
 
